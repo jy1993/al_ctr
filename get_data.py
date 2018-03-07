c35fd73a0a64857160c09aa4ac4119d4ec684b9e
jy1993 <jiangyan@utexas.edu>
2018-03-07 15:38:30 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,4 +1,5 @@
 from torch.utils.data import dataset
+import pandas as pd
 
 
 class Ali(dataset):
@@ -6,10 +7,18 @@ class Ali(dataset):
 	def __init__(self, arg):
 		super(Ali, self).__init__()
 		self.arg = arg
+		if self.arg.test_flag:
+			d_path = self.arg.data_path + '/test'	
+		elif self.arg.train_flag:
+			d_path = self.arg.data_path + '/train'		
+		else: 
+			d_path = self.arg.data_path + '/val'
+		self.cdata = open(f'{d_path}').read().split('\n')
 
 	def __getitem__(self, index):
-		pass
+		data_and_label = self.cdata[index].split(None)
+		data, label = data_and_label[:-1], data_and_label[-1]
+		return data, label
 
 	def __len__(self):
-		pass
-		
+		return len(self.cdata)		
